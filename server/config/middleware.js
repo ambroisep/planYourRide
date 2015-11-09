52aea6db62f80780f8160cd5ee7602efcbf776aa
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-09 14:12:46 -0800

    serving favicon

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,10 @@
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 module.exports = function (app, express) {
 
   app.use(bodyParser.json());
-  
-}
+  app.use(favicon(__dirname + '/../../client/assets/favicon.ico'));
+  app.use(express.static(__dirname + '/../../client'));
+
+};
