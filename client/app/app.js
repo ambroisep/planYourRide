bd0d07e7d98bc2adbf2ba69d3106f1f2abab61f2
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-09 12:14:09 -0800

    added tripPlanner to angular dependecies, and started client router

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,12 @@
-angular.module('planYourRide', []);
+angular.module('planYourRide', ['planYourRide.tripPlanner'])
+  
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/tripPlanner/tripPlanner.html',
+      controller: 'TripPlanner'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
