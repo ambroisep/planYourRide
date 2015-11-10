719bb1597a32a57d8d99a3e9155e32ff76d3d8ba
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-10 15:38:54 -0800

    removed end point from options

--- client/app/tripPlanner/tripPlanner.js
+++ client/app/tripPlanner/tripPlanner.js
@@ -4,8 +4,10 @@ angular.module('planYourRide.tripPlanner', [])
 
   $scope.tripData = {};
   $scope.submitTrip = function () {
-    $scope.tripData.startPoint = $scope.tripData.startPoint || 'San Francisco';
-    $scope.tripData.endPoint = $scope.tripData.endPoint || 'Sausalito';
+    $scope.tripData.startPoint = $scope.tripData.startPoint || '944 Market Street, San Francisco, CA';
+    $scope.tripData.endPoint = $scope.tripData.startPoint;
+    $scope.tripData.hours = $scope.tripData.hours || 1;
+    $scope.tripData.minutes = $scope.tripData.minutes || 0;
     Directions.postDemand($scope.tripData)
       .then(function (resp) {
         $location.path('/direction');
