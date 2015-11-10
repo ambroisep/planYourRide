6f899ae4b15c7b6b2e14b37be44355b9cb601338
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-09 16:47:30 -0800

    first working call to googleMapsAPI working

---
+++ server/GMapsAPI/itinerary.js
@@ -0,0 +1,16 @@
+var httpAsynch = require('request-promise');
+
+var url = 'https://maps.googleapis.com/maps/api/directions/json';
+
+exports.getItinerary = function(params, callback) {
+  var parameters = {
+    uri: url,
+    qs: {
+      origin: params.origin,
+      destination: params.destination,
+      key: 'AIzaSyBQs4N37ZBvQGPqcYdZVKUPTvfHS-AKLZQ'
+    },
+    json: true
+}
+  return httpAsynch(parameters);
+};
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,10 +1,18 @@
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+var Gmaps = require('./../GMapsAPI/itinerary');
 
 module.exports = function (app, express) {
 
   app.use(bodyParser.json());
   app.use(favicon(__dirname + '/../../client/assets/favicon.ico'));
   app.use(express.static(__dirname + '/../../client'));
-
+  app.get('/test', function (req, res) {
+    Gmaps.getItinerary({origin: 'San Frncisco, CA',
+      destination: 'Los Angeles, CA'})
+      .then(function(itinerary) {
+        console.log(itinerary);
+        res.send(itinerary);
+      });
+  });
 };
